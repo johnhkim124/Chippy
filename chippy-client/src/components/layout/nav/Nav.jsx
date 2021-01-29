f69a68b74f437bc3118cd29eb8928eeb00448999
John <johnhkim124@yahoo.com>
2021-01-29 09:53:35 -0500

    added sign in links and auth.js

--- chippy-client/src/components/layout/nav/Nav.jsx
+++ chippy-client/src/components/layout/nav/Nav.jsx
@@ -1,22 +1,34 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = (props) => {
+  const { currentUser, handleLogout } = props;
   return (
     <>
       <div className="nav-container">
         <nav>
-          <ul>
-            <Link to="/snacks">
-              <ul>Explore</ul>
-            </Link>
-            <Link to="/countries">
-              <ul>Countries</ul>
-            </Link>
-            <Link to="/flavors">
-              <ul>Flavors</ul>
-            </Link>
-          </ul>
+          <h1>Brand</h1>
+          {currentUser ? (
+            <>
+              <p>{currentUser.username}</p>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </>
+          )}
+          {currentUser && <></>}
+          <Link to="/snacks">
+            <ul>Explore</ul>
+          </Link>
+          <Link to="/countries">
+            <ul>Countries</ul>
+          </Link>
+          <Link to="/flavors">
+            <ul>Flavors</ul>
+          </Link>
         </nav>
       </div>
     </>
