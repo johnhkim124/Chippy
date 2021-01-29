f1bf36b41bcb5f7addd47b8dc98355ff5ef47427
John <johnhkim124@yahoo.com>
2021-01-29 10:47:11 -0500

    added registerUser, verifyUser, removeToken in auth.js

---
+++ chippy-client/src/screens/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Login(props) {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = formData;
+
+  return <form></form>;
+}
