d2f026045a5bccfc53ddeafc31ebe743675016f0
John <johnhkim124@yahoo.com>
2021-02-02 20:16:49 -0500

    css carousel singlesnacks

--- chippy-client/src/components/carousel/Carousel.jsx
+++ chippy-client/src/components/carousel/Carousel.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./carousel.css";
 
 export default class MultipleItems extends Component {
   render() {
@@ -16,32 +17,39 @@ export default class MultipleItems extends Component {
       <div>
         <h2> Multiple items </h2>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+          <div className="carousel1">
+            <img
+              className="carousel carousel1-img"
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.-ErRTzpZKQ2UiCfA9lxBWQHaHa%26pid%3DApi&f=1"
+              alt="chips variety"
+            />
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="carousel carousel2">
+            <img
+              className="carousel2-img"
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.y6kRTv7zQdS7IwuTS0WOXwAAAA%26pid%3DApi&f=1"
+              alt="candy variety"
+            />
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="carousel carousel3">
+            <img />
           </div>
-          <div>
-            <h3>4</h3>
+          <div className="carousel carousel4">
+            <img
+              className="carousel4-img"
+              src="https://m.media-amazon.com/images/I/81IiZskCBZL._AC_UL320_.jpg"
+              alt="valentine assorted chocolate"
+            />
           </div>
-          <div>
-            <h3>5</h3>
+          <div className="carousel carousel5">
+            <img
+              className="carousel5-img"
+              src="https://m.media-amazon.com/images/I/81zkBbZZawL._AC_UL320_.jpg"
+              alt="popcorn valentine"
+            />
           </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
+          <div className="carousel carousel6">
+            <img />
           </div>
         </Slider>
       </div>
